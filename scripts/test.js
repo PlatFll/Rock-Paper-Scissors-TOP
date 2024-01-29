e30fca93e16111098308693a909ee91161000ec9
PlatFll <PlatooFl@gmail.com>
2024-01-29 17:56:19 +0300

    Small list DOM excersise

--- scripts/test.js
+++ scripts/test.js
@@ -37,7 +37,19 @@ newDiv.appendChild(divP);
 container.appendChild(newDiv);
 
 btn.addEventListener('click', function (e) {
-    console.log(e.target);
+    e.target.style.background = 'blue';
   });
+
+  // buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
 
 
