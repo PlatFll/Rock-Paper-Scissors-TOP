48f3fb15bd7977bb2a1c4b489a0e90133c858dd6
PlatFll <PlatooFl@gmail.com>
2024-02-01 18:03:09 +0300

    Game works using buttons
    The game now works using buttons and displays the winner text on the webpage

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+//BUTTONS
+const playedRockButton = document.querySelector("#rock");
+const playedPaperButton = document.querySelector("#paper");
+const playedScissorsButton = document.querySelector("#scissors");
+
+
+//Buttons function
+playedRockButton.addEventListener("click" , () => {playRound("Rock", getComputerChoice())});
+playedPaperButton.addEventListener("click" , () => {playRound("Paper", getComputerChoice())});
+playedScissorsButton.addEventListener("click" , () => {playRound("Scissors", getComputerChoice())});
+
+//Game outcome text
+const container = document.querySelector('#container');
+
+const gameOutcome = document.createElement('div');
+gameOutcome.classList.add('gameOutcome');
+gameOutcome.textContent = "";
+
+container.appendChild(gameOutcome);
+
+
+const computerChoice = document.createElement('div');
+container.appendChild(computerChoice);
+
+
+//Functions
+
 function getComputerChoice () {
     let randomChoice = "";
     let num = Math.floor(Math.random() * 10);
@@ -13,6 +40,15 @@ function getComputerChoice () {
     return randomChoice;
 }  
 
+function announceComputerChoice() {
+    let choice = getComputerChoice();
+
+    if(choice == "Rock"){
+        computerChoice.textContent = "Computer played rock!"
+    }
+}
+
+
 function playRound (playerSelection, computerSelction) {
 
     let win = 0;
@@ -21,6 +57,7 @@ function playRound (playerSelection, computerSelction) {
     if(((playerSelection == "Rock") && (computerSelction == "Rock")) || ((playerSelection == "Paper") && (computerSelction == "Paper")) || 
     ((playerSelection == "Scissors") && (computerSelction == "Scissors"))) {
         console.log("It's a tie!");
+        gameOutcome.textContent = "It's a tie!";
         win = 0
         return;
     }
@@ -28,16 +65,19 @@ function playRound (playerSelection, computerSelction) {
     // THE COMPUTER WINS
     else if ((playerSelection == "Scissors") && (computerSelction == "Rock")) {
         console.log("Computer wins! Rock beats Scissors!");
+        gameOutcome.textContent = "Computer wins! Rock beats Scissors!";
         win = 1;
         return win;
     }
     else if ((playerSelection == "Paper") && (computerSelction == "Scissors")) {
         console.log("Computer wins! Scissors beat Paper!");
+        gameOutcome.textContent = "Computer wins! Scissors beat Paper!";
         win = 1;
         return win;
     }
     else if ((playerSelection == "Rock") && (computerSelction == "Paper")) {
         console.log("Computer wins! Paper beats Rock!");
+        gameOutcome.textContent = "Computer wins! Paper beats Rock!";
         win = 1;
         return win;
     }
@@ -45,50 +85,63 @@ function playRound (playerSelection, computerSelction) {
     // THE PLAYER WINS
     else if ((playerSelection == "Rock") && (computerSelction == "Scissors")) {
         console.log("Player wins! Rock beats Scissors!");
+        gameOutcome.textContent = "Player wins! Rock beats Scissors!";
         win = 2;
         return win;
     }
     else if ((playerSelection == "Scissors") && (computerSelction == "Paper")) {
         console.log("Player wins! Scissors beat Paper!");
+        gameOutcome.textContent = "Player wins! Scissors beat Paper!";
         win = 2;
         return win;
     }
     else if ((playerSelection == "Paper") && (computerSelction == "Rock")) {
         console.log("Player wins! Paper beats Rock!");
+        gameOutcome.textContent = "Player wins! Paper beats Rock!";
         win = 2;
         return win;
     }
 }
 
-function gameToFive(win) {
-    let playerScore = 0;
-    let computerScore = 0;
+// function gameToFive(win) {
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    for(let i = 0; i < 5; i++) {
-        let playerSelection = prompt("What's your choice? Rock, Paper or Scissors!?: ");
-        let computerSelction = getComputerChoice();
-        const win = playRound(playerSelection, computerSelction);
+//     for(let i = 0; i < 5; i++) {
+//         let playerSelection = prompt("What's your choice? Rock, Paper or Scissors!?: ");
+//         let computerSelction = getComputerChoice();
+//         const win = playRound(playerSelection, computerSelction);
+//         if(win == 2){
+//             playerScore++;
+//         }
+//         else if(win == 1) {
+//             computerScore++;
+//         }
+//     }
+
+
+//     if((playerScore > computerScore))
+//         return "Player Wins overall!";
+//     else if ((computerScore > playerScore))
+//         return "Computer wins overall!";
+//     else 
+//         return "It's a draw overall!"
+
+// }
+
+//REDUNDANT FUNCTION FOR NOW
+function customPlay () {
+    let playerScore = 0;
+    let computerScore = 0; 
+
+    let computerSelction = getComputerChoice();
+    let playerSelection = getPlayerChoice();
+
+    const win = playRound(playerSelection, computerSelction);
         if(win == 2){
             playerScore++;
         }
         else if(win == 1) {
             computerScore++;
         }
-    }
-
-
-    if((playerScore > computerScore))
-        return "Player Wins overall!";
-    else if ((computerScore > playerScore))
-        return "Computer wins overall!";
-    else 
-        return "It's a draw overall!"
-
 }
-
-// console.log(getComputerChoice());
-
-
-
-//console.log(playRound(playerSelection, computerSelction))
-console.log(gameToFive(playRound()));
